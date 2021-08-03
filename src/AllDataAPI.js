25c8198c1bd795d61a6db49436d1ab09c22f2e56
mariamalshammari <malshammari37@gmail.com>
2021-08-03 13:01:14 +0300

    mm

--- src/AllDataAPI.js
+++ src/AllDataAPI.js
@@ -1,14 +1,71 @@
 import React, { Component } from 'react';
 import { withAuth0 } from '@auth0/auth0-react';
+import axios from 'axios';
+import MyCard from './components/MyCard'
+import { Card, Button } from 'react-bootstrap';
 
 
 class AllDataAPI extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            colorsArr: [],
+            email: ''
+        }
+    }
+
+    componentDidMount = async () => {
+        try{
+        const { user, isAuthenticated } = this.props.auth0;
+        let url = `${process.env.REACT_APP_SERVER}/colors`;
+        let resData = await axios.get(url)
+        this.setState({
+            colorsArr: resData.data,
+            email: user.email
+        })
+    }
+    catch(error){
+        console.log('error');
+    }
+    }
+
+    addfav=async(item)=>{
+        try{
+        let url=`${process.env.REACT_APP_SERVER}/addfav/${this.state.email}`
+        let resData = await axios.post(url,item)
+        this.setState({
+            colorsArr: resData.data,
+        })}
+        catch(error){
+            console.log('error');
+        }   
+    }
     render() {
         return (
             <div>
                 <h1>All Data from the API</h1>
-                <h3>Select your favorites :)</h3>
-            </div>
+                <h3>Select your favorites :)</h3> 
+
+                {/* <MyCard colorsArr={this.state.colorsArr} /> */}
+
+                    {
+                        this.state.colorsArr.map((item) => {
+                            return (
+
+                                <Card style={{ width: '18rem' }}>
+                                    <Card.Img variant="top" src="holder.js/100px180" />
+                                    <Card.Body>
+                                        <Card.Title>{item.title}</Card.Title>
+                                        <Card.Text>
+                                            {/* <img src={`${item.imageUrl}`}/> */}
+                                        </Card.Text>
+                                        <Button variant="primary" onClick={()=>{this.addfav(item)}}>add to fav</Button>
+                                    </Card.Body>
+                                </Card>
+
+                            )
+                         })}
+                         </div> 
         )
     }
 }
--- src/Profile.js
+++ src/Profile.js
@@ -4,6 +4,7 @@ import { withAuth0 } from '@auth0/auth0-react';
 class Profile extends Component {
     render() {
         const { user, isAuthenticated } = this.props.auth0;
+        console.log(user);
         return (
             <> { isAuthenticated &&
                 <div>
